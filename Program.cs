8857dabdb72de2700c9598674bc5ff52abb5ec90
Benison Iteh <BIteh@Custodianplc.com.ng>
2023-10-17 14:26:20 +0100

    v1.0.1 Updates

--- Program.cs
+++ Program.cs
@@ -2,14 +2,47 @@
 using System;
 using System.Drawing;
 using System.Security.Principal;
-using Excel = Microsoft.Office.Interop.Excel;
+using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
 
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Welcome to C# Training");
+        PATA.ClassA.Print();
+        PATB.ClassA.Print();
+    }
+}
 
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print Method");
+            }
+        }
+      
+    }
+}
+
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team B Print Method");
+            }
+        }
+      
     }
 }
     
